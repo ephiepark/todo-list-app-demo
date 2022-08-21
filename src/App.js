3f1da3e1d54233c9831898bf966ff6cd4d74aed0
Ephraim Park <ephiepark@gmail.com>
2022-08-21 12:56:47 -0700

    [2/n] Create TodoItem react component: Strikethrough todo item if marked finished

--- src/App.js
+++ src/App.js
@@ -23,14 +23,20 @@ const TodoItemInputField = (props) => {
 };
 
 const TodoItem = (props) => {
+  const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {};
   return (<li>
-    <span>{props.todoItem.todoItemContent}</span>
+    <span
+      style={style}
+      onClick={() => props.onTodoItemClick(props.todoItem)}
+    >
+      {props.todoItem.todoItemContent}
+    </span>
   </li>);
 };
 
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <TodoItem key={todoItem.id} todoItem={todoItem} />;
+    return <TodoItem key={todoItem.id} todoItem={todoItem} onTodoItemClick={props.onTodoItemClick} />;
   });
 
   return (<div>
@@ -51,10 +57,25 @@ function App() {
     }]);
   };
 
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(todoItemList.map((todoItem) => {
+      if (clickedTodoItem.id === todoItem.id) {
+        return {
+          id: clickedTodoItem.id,
+          todoItemContent: clickedTodoItem.todoItemContent,
+          isFinished: !clickedTodoItem.isFinished,
+        };
+      } else {
+        return todoItem;
+      }
+    }));
+  };
+
+
   return (
     <div className="App">
       <TodoItemInputField onSubmit={onSubmit} />
-      <TodoItemList todoItemList={todoItemList} />
+      <TodoItemList todoItemList={todoItemList} onTodoItemClick={onTodoItemClick}/>
     </div>
   );
 }
