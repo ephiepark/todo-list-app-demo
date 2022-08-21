9cc99736821b5f7ba206e93441f626db1b50ef3b
Ephraim Park <ephiepark@gmail.com>
2022-08-21 12:52:46 -0700

    [1/n] Create TodoItem react component: Create a placeholder component & display todo item

--- src/App.js
+++ src/App.js
@@ -22,9 +22,15 @@ const TodoItemInputField = (props) => {
   </div>);
 };
 
+const TodoItem = (props) => {
+  return (<li>
+    <span>{props.todoItem.todoItemContent}</span>
+  </li>);
+};
+
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <li key={index}>{todoItem.todoItemContent}</li>;
+    return <TodoItem key={todoItem.id} todoItem={todoItem} />;
   });
 
   return (<div>
