99c724c9c0bd5f603a8a82fe72fb61b049d13400
Ephraim Park <ephiepark@gmail.com>
2022-08-21 13:26:41 -0700

    [1/n] Integrate Firebase Login with Google: Add App Bar UI for sign in/out flow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -78,6 +81,20 @@ const TodoItemList = (props) => {
   </div>);
 };
 
+const TodoListAppBar = (props) => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Todo List App
+        </Typography>
+        <Button color="inherit">Log In</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
@@ -124,6 +141,7 @@ function App() {
 
   return (
     <div className="App">
+      <TodoListAppBar />
       <TodoItemInputField onSubmit={onSubmit} />
       <TodoItemList
         todoItemList={todoItemList}
